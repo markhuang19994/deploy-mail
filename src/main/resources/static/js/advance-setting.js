aa30d9511a701f5201ba523e91e1dfd90d59d829
mark <markhuang1993@gmail.com>
2019-09-06 18:18:30 +0800

    [Fix]pwd input value bug

--- src/main/resources/static/js/advance-setting.js
+++ src/main/resources/static/js/advance-setting.js
@@ -1,38 +1,34 @@
-let isPasswordTrigger = false;
-
 function fillAdvanceSettingValue() {
-    isPasswordTrigger = false
     document.getElementById('setting-mail-account').value = userMailAccount;
     document.getElementById('setting-mail-password').value = '********';
 }
 
 $('#setting-mail-password').click(e => {
-    if (!isPasswordTrigger) {
-        e.currentTarget.value = '';
-    }
-    isPasswordTrigger = true;
+    e.currentTarget.value = '';
 });
 
-$('#advance-setting-save-btn').click(() => {
+$('#advance-setting-save-btn').click( async () => {
     const account = document.getElementById('setting-mail-account');
     const pwd = document.getElementById('setting-mail-password');
-
-    $.ajax({
-        type: 'POST',
-        url: 'mailHandler/saveAdvanceMailSetting',
-        data: {
-            account: account.value,
-            pwd: !isPasswordTrigger ? '' : pwd.value
-        },
-        success: (d) => alert(d),
-        error: e => {
-            console.log(e['responseJSON']);
-            alert(e.message);
-        },
-        done: () => {
-            changePageBottomIn($('#advance-setting-body'), $('#step1-body'));
-        }
+    const d = await new Promise(res => {
+        $.ajax({
+            type: 'POST',
+            url: 'mailHandler/saveAdvanceMailSetting',
+            data: {
+                account: account.value,
+                pwd: pwd.value === '********' ? '' : pwd.value
+            },
+            success: (d) => {
+                res(d)
+            },
+            error: e => {
+                console.log(e['responseJSON']);
+                res(e);
+            }
+        });
     });
+    changePageBottomIn($('#advance-setting-body'), $('#step1-body'));
+    setTimeout(() => alert(d), 500)
 });
 
 $('#advance-setting-cancel-btn').click(() => {
