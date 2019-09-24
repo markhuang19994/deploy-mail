60d04635a2c812512e91fe01f364546ec28293da
mark <markhuang1993@gmail.com>
2019-09-24 15:56:33 +0800

    [Add]background image feature

---
+++ src/main/resources/static/js/background-image.js
@@ -0,0 +1,9 @@
+$(function () {
+    const body = document.body;
+    Object.assign(body.style, {
+        backgroundImage: 'url(/image/bg01.jpg)',
+        backgroundClip: 'padding-box',
+        backgroundSize: 'cover',
+        zIndex: '-10'
+    });
+});
